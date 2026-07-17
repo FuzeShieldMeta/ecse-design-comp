1e44cce5f3e86d244c2308d8f4b3dd17d240396e
FuzeShieldMeta <264532820+FuzeShieldMeta@users.noreply.github.com>
2026-07-17 13:49:30 +1200

    feat: test daisy chained panels

--- src/1_SimpleTestShapes.ino
+++ src/1_SimpleTestShapes.ino
@@ -1,14 +1,20 @@
 
-// Example sketch which shows how to display some patterns
-// on a 64x32 LED matrix
+// Example sketch which shows how to display some patterns on two
+// vertically stacked 64x32 panels as one 64x64 display.
 //
 
-#include <ESP32-HUB75-MatrixPanel-I2S-DMA.h>
+#include <ESP32-HUB75-VirtualMatrixPanel_T.hpp>
 
 
 #define PANEL_RES_X 64      // Number of pixels wide of each INDIVIDUAL panel module. 
 #define PANEL_RES_Y 32     // Number of pixels tall of each INDIVIDUAL panel module.
-#define PANEL_CHAIN 1      // Total number of panels chained one to another
+#define PANEL_ROWS  2      // Two panels stacked vertically
+#define PANEL_COLS  1
+#define PANEL_CHAIN (PANEL_ROWS * PANEL_COLS)
+
+// Panel 1 starts at the top-right and its OUT connector feeds panel 2.
+// Change this if your physical input connector is in a different corner.
+#define PANEL_CHAIN_TYPE CHAIN_TOP_RIGHT_DOWN
 
 // HUB75 wiring for a 38-pin ESP32-WROOM-32 development board.
 // These GPIOs avoid the flash bus (6-11), input-only pins (34-39),
@@ -30,6 +36,7 @@
  
 //MatrixPanel_I2S_DMA dma_display;
 MatrixPanel_I2S_DMA *dma_display = nullptr;
+VirtualMatrixPanel_T<PANEL_CHAIN_TYPE> *display = nullptr;
 
 uint16_t myBLACK, myWHITE, myRED, myGREEN, myBLUE;
 
@@ -38,13 +45,13 @@ uint16_t myBLACK, myWHITE, myRED, myGREEN, myBLUE;
 // From: https://gist.github.com/davidegironi/3144efdc6d67e5df55438cc3cba613c8
 uint16_t colorWheel(uint8_t pos) {
   if(pos < 85) {
-    return dma_display->color565(pos * 3, 255 - pos * 3, 0);
+    return display->color565(pos * 3, 255 - pos * 3, 0);
   } else if(pos < 170) {
     pos -= 85;
-    return dma_display->color565(255 - pos * 3, 0, pos * 3);
+    return display->color565(255 - pos * 3, 0, pos * 3);
   } else {
     pos -= 170;
-    return dma_display->color565(0, pos * 3, 255 - pos * 3);
+    return display->color565(0, pos * 3, 255 - pos * 3);
   }
 }
 
@@ -52,53 +59,53 @@ void drawText(int colorWheelOffset)
 {
   
   // draw text with a rotating colour
-  dma_display->setTextSize(1);     // size 1 == 8 pixels high
-  dma_display->setTextWrap(false); // Don't wrap at end of line - will do ourselves
+  display->setTextSize(1);     // size 1 == 8 pixels high
+  display->setTextWrap(false); // Don't wrap at end of line - will do ourselves
 
-  dma_display->setCursor(5, 0);    // start at top left, with 8 pixel of spacing
+  display->setCursor(5, 0);    // start at top left, with 8 pixel of spacing
   uint8_t w = 0;
   const char *str = "ESP32 DMA";
   for (w=0; w<strlen(str); w++) {
-    dma_display->setTextColor(colorWheel((w*32)+colorWheelOffset));
-    dma_display->print(str[w]);
+    display->setTextColor(colorWheel((w*32)+colorWheelOffset));
+    display->print(str[w]);
   }
 
-  dma_display->println();
-  dma_display->print(" ");
+  display->println();
+  display->print(" ");
   for (w=9; w<18; w++) {
-    dma_display->setTextColor(colorWheel((w*32)+colorWheelOffset));
-    dma_display->print("*");
+    display->setTextColor(colorWheel((w*32)+colorWheelOffset));
+    display->print("*");
   }
   
-  dma_display->println();
+  display->println();
 
-  dma_display->setTextColor(dma_display->color444(15,15,15));
-  dma_display->println("LED MATRIX!");
+  display->setTextColor(display->color444(15,15,15));
+  display->println("LED MATRIX!");
 
   // print each letter with a fixed rainbow color
-  dma_display->setTextColor(dma_display->color444(0,8,15));
-  dma_display->print('3');
-  dma_display->setTextColor(dma_display->color444(15,4,0));
-  dma_display->print('2');
-  dma_display->setTextColor(dma_display->color444(15,15,0));
-  dma_display->print('x');
-  dma_display->setTextColor(dma_display->color444(8,15,0));
-  dma_display->print('6');
-  dma_display->setTextColor(dma_display->color444(8,0,15));
-  dma_display->print('4');
+  display->setTextColor(display->color444(0,8,15));
+  display->print('6');
+  display->setTextColor(display->color444(15,4,0));
+  display->print('4');
+  display->setTextColor(display->color444(15,15,0));
+  display->print('x');
+  display->setTextColor(display->color444(8,15,0));
+  display->print('6');
+  display->setTextColor(display->color444(8,0,15));
+  display->print('4');
 
   // Jump a half character
-  dma_display->setCursor(34, 24);
-  dma_display->setTextColor(dma_display->color444(0,15,15));
-  dma_display->print("*");
-  dma_display->setTextColor(dma_display->color444(15,0,0));
-  dma_display->print('R');
-  dma_display->setTextColor(dma_display->color444(0,15,0));
-  dma_display->print('G');
-  dma_display->setTextColor(dma_display->color444(0,0,15));
-  dma_display->print("B");
-  dma_display->setTextColor(dma_display->color444(15,0,8));
-  dma_display->println("*");
+  display->setCursor(34, 24);
+  display->setTextColor(display->color444(0,15,15));
+  display->print("*");
+  display->setTextColor(display->color444(15,0,0));
+  display->print('R');
+  display->setTextColor(display->color444(0,15,0));
+  display->print('G');
+  display->setTextColor(display->color444(0,0,15));
+  display->print("B");
+  display->setTextColor(display->color444(15,0,8));
+  display->println("*");
 
 }
 
@@ -138,38 +145,44 @@ void setup() {
   dma_display->setBrightness8(90); //0-255
   dma_display->clearScreen();
 
-  myBLACK = dma_display->color565(0, 0, 0);
-  myWHITE = dma_display->color565(255, 255, 255);
-  myRED = dma_display->color565(255, 0, 0);
-  myGREEN = dma_display->color565(0, 255, 0);
-  myBLUE = dma_display->color565(0, 0, 255);
+  // Convert the electrical 128x32 chain into a 64x64 drawing surface.
+  display = new VirtualMatrixPanel_T<PANEL_CHAIN_TYPE>(
+    PANEL_ROWS, PANEL_COLS, PANEL_RES_X, PANEL_RES_Y
+  );
+  display->setDisplay(*dma_display);
+
+  myBLACK = display->color565(0, 0, 0);
+  myWHITE = display->color565(255, 255, 255);
+  myRED = display->color565(255, 0, 0);
+  myGREEN = display->color565(0, 255, 0);
+  myBLUE = display->color565(0, 0, 255);
   
 
-  dma_display->fillScreen(myWHITE);
+  display->fillScreen(myWHITE);
   
   // fix the screen with green
-  dma_display->fillRect(0, 0, dma_display->width(), dma_display->height(), dma_display->color444(0, 15, 0));
+  display->fillRect(0, 0, display->width(), display->height(), display->color444(0, 15, 0));
   delay(500);
 
   // draw a box in yellow
-  dma_display->drawRect(0, 0, dma_display->width(), dma_display->height(), dma_display->color444(15, 15, 0));
+  display->drawRect(0, 0, display->width(), display->height(), display->color444(15, 15, 0));
   delay(500);
 
   // draw an 'X' in red
-  dma_display->drawLine(0, 0, dma_display->width()-1, dma_display->height()-1, dma_display->color444(15, 0, 0));
-  dma_display->drawLine(dma_display->width()-1, 0, 0, dma_display->height()-1, dma_display->color444(15, 0, 0));
+  display->drawLine(0, 0, display->width()-1, display->height()-1, display->color444(15, 0, 0));
+  display->drawLine(display->width()-1, 0, 0, display->height()-1, display->color444(15, 0, 0));
   delay(500);
 
   // draw a blue circle
-  dma_display->drawCircle(10, 10, 10, dma_display->color444(0, 0, 15));
+  display->drawCircle(10, 10, 10, display->color444(0, 0, 15));
   delay(500);
 
   // fill a violet circle
-  dma_display->fillCircle(40, 21, 10, dma_display->color444(15, 0, 15));
+  display->fillCircle(40, 21, 10, display->color444(15, 0, 15));
   delay(500);
 
   // fill the screen with 'black'
-  dma_display->fillScreen(dma_display->color444(0, 0, 0));
+  display->fillScreen(display->color444(0, 0, 0));
 
   //drawText(0);
 
